9f1bbeba0b36cec517ea0e8097be4f40bd527a67
langaludidi <langaludidi@gmail.com>
2025-08-29 11:36:07 +0200

    Add missing main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App'
+import './index.css'
 
-// global styles
-import "./index.css";
-import "./App.css";
+const queryClient = new QueryClient()
 
-// Toast provider (you already have components/Toast with a context)
-import { ToastProvider } from "./components/Toast";
-
-// Create one query client for the app
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      staleTime: 30_000
-    }
-  }
-});
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ToastProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ToastProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
