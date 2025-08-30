d0cf09fc33f5586949d9cb2478f5714545d91ac4
langaludidi <66078749+langaludidi@users.noreply.github.com>
2025-08-30 14:59:17 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
- import React from 'react'
-  import ReactDOM from 'react-dom/client'
-  import { BrowserRouter } from 'react-router-dom'
-  import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-  import App from './App'
-  import './index.css'
+ import { Routes, Route, Navigate } from "react-router-dom";
+  import AppShell from "./components/layout/AppShell";
+  import Protected from "./components/Protected";
+  import RequireRole from "./components/RequireRole";
+  import Home from "./routes/Home";
+  import SignIn from "./routes/SignIn";
+  import SuperAdmin from "./routes/SuperAdmin";
 
-  const queryClient = new QueryClient()
+  export default function App() {
+    return (
+      <AppShell>
+        <Routes>
+          <Route path="/" element={<Protected><Home /></Protected>} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/super-admin" element={<SuperAdmin />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AppShell>
+    );
+  }
 
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </React.StrictMode>,
-  )
